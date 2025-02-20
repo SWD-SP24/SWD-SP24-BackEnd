32a62d0f2fe203716ac75c77371bd9a4b1087c9f
Nguyen Trung Hung <nguyentrunghung03@gmail.com>
2025-02-20 15:57:00 +0700

    test github action

--- Controllers/AdminAuthController.cs
+++ Controllers/AdminAuthController.cs
@@ -108,7 +108,7 @@ namespace SWD392.Controllers
         /// <remarks>
         /// Errors:
         /// - Email already exists.
-        /// - Fail to create account (FB)  Check for valid email.
+        /// - Fail to create account (FB)  Check for valid email. 
         /// - Fail to create account (DB)
         /// </remarks>
         /// <response code="200">User register</response>
