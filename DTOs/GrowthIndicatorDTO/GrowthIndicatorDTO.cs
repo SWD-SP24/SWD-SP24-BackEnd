2710db77762ed98a5035a1dd6aec02da167ee181
Hung <hungntse182394@fpt.edu.vn>
2025-02-25 17:32:53 +0700

    prelim growthindicator editor

---
+++ DTOs/GrowthIndicatorDTO/GrowthIndicatorDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace SWD392.DTOs.GrowthIndicatorDTO
+{
+    public class GrowthIndicatorDTO
+    {
+        public int GrowthIndicatorsId { get; set; }
+        public int Height { get; set; }
+        public int Weight { get; set; }
+        public int Bmi { get; set; }
+        public int ChildrenId { get; set; }
+
+    }
+}
