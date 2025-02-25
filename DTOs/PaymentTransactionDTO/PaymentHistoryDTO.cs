23cf93e15f7885d79dc3df86d10f7a4f0c6c296f
KS_REACT_SE170505 <anntse170505@fpt.edu.vn>
2025-02-25 12:07:13 +0700

    view payment history for member

---
+++ DTOs/PaymentTransactionDTO/PaymentHistoryDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace SWD392.DTOs.PaymentTransactionDTO
+{
+    public class PaymentHistoryDTO
+    {
+        public string PaymentId { get; set; }
+        public int UserId { get; set; }
+        public int MembershipPackageId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime TransactionDate { get; set; }
+    }
+}
