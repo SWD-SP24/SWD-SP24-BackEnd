5e039cb47d34adf7bd577f33a84924b44aff3065
Nguyen Trung Hung <nguyentrunghung03@gmail.com>
2025-02-27 18:32:12 +0700

    Update AddChildDTO.cs

--- DTOs/ChildDTO/AddChildDTO.cs
+++ DTOs/ChildDTO/AddChildDTO.cs
@@ -3,7 +3,6 @@
     public class AddChildDTO
     {
         public required string FullName { get; set; }
-        public required int Age { get; set; }
         public string? Avatar { get; set; }
         public required DateOnly Dob { get; set; }
         public string? BloodType { get; set; }
