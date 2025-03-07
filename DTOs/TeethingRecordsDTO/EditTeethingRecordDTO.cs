6232eb3768d93712b457f1c3438586749148686e
Nguyen Trung Hung <nguyentrunghung03@gmail.com>
2025-03-07 23:38:56 +0700

    Fix

--- DTOs/TeethingRecordsDTO/EditTeethingRecordDTO.cs
+++ DTOs/TeethingRecordsDTO/EditTeethingRecordDTO.cs
@@ -2,6 +2,7 @@
 {
     public class EditTeethingRecordDTO
     {
+        public int ToothNumber { get; set; } // Change to ToothNumber
         public DateTime? EruptionDate { get; set; }
         public DateTime? RecordTime { get; set; }
         public string? Note { get; set; } // Add this line
